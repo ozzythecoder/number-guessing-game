51725351a18298af304c8de2a381307fafddfe27
August McAllister <august.chm@gmail.com>
2022-11-03 11:15:52 -0500

    Server receives and stores inputs correctly

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -14,24 +14,42 @@ function render() {
   }).then( (response) => {
     $('#guess-output').empty();
 
-    for (let guess of response) {
+    for (let guesses of response) {
       $('#guess-output').append(`
         <tr>
-          <td>${guess.numGuessed}</td>
-          <td>${guess.name}</td>
-          <td>${guess.hiLo}</td>
+          <td>${guesses.augustGuess}</td>
+          <td>${guesses.augustHiLo}</td>
+          <td>August</td>
         </tr>
-      `)
-    }
+        <tr>
+          <td>${guesses.jaredGuess}</td>
+          <td>${guesses.jaredHiLo}</td>
+          <td>Jared</td>
+        </tr>
+      `) // end append
+    } // end for loop
 
     console.log('we got it!', response);
   }).catch( (err) => {
     console.log('shit!');
-  })
+  }) // end ajax
 
 }
 
 function storeGuess() {
+  console.log('in storeGuess');
 
+  $.ajax({
+    method: 'POST',
+    url: '/guesses',
+    data: {
+      augustGuess: $('#august-guess').val(),
+      jaredGuess: $('#jared-guess').val()
+    }
+  }).then( (res) => {
+    console.log('success!');
+  }).catch( (err) => {
+    console.log('whyyyyy 😭');
+  }) // end ajax
 
 }
--- server/server.js
+++ server/server.js
@@ -9,25 +9,51 @@ app.use(bodyParser.urlencoded({extended:true}))
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 const guessArray = [{
-    numGuessed: 5,
-    name: 'August',
-    hiLo: 'high' },
-  {
-    numGuessed: 9,
-    name: 'Jared',
-    hiLo: 'high'
-  }
+  augustGuess: 9,
+  augustHiLo: null,
+  jaredGuess: 12,
+  jaredHiLo: null
+}
 ];
 
 function getRandomInteger(min, max) {
   return Math.floor(Math.random() * (1 + max - min) + min);
 }
 
+let theNumber = getRandomInteger(1, 25);
+console.log('The number is:', theNumber);
+
 // GET & POST Routes go here
 app.get('/guesses', (req, res) => {
   console.log('retrieving guesses');
   console.log(guessArray);
   res.send(guessArray);
+})
+
+app.post('/guesses', (req, res) => {
+  console.log('posting guesses');
+
+  let guessObj = req.body;
+  if (guessObj.augustGuess > theNumber) {
+    guessObj.augustHiLo = 'High'
+  } else if (guessObj.augustGuess < theNumber) {
+    guessObj.augustHiLo = 'Low'
+  } else if (guessObj.augustGuess == theNumber) {
+    guessObj.augustHiLo = 'Winner!'
+  }
+
+  if (guessObj.jaredGuess > theNumber) {
+    guessObj.jaredHiLo = 'High'
+  } else if (guessObj.jaredGuess < theNumber) {
+    guessObj.jaredHiLo = 'Low'
+  } else if (guessObj.jaredGuess == theNumber) {
+    guessObj.jaredHiLo = 'Winner!'
+  }
+
+  guessArray.push(guessObj);
+  console.log('the latest guesses are:', guessObj);
+
+  res.sendStatus(200);
 })
 
 app.listen(PORT, () => {
