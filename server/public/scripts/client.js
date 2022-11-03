fa5e43e8416b082a065148f7033ea7a806e76e16
August McAllister <august.chm@gmail.com>
2022-11-03 10:08:29 -0500

    Server and client are talking

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,19 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!")
+
+  render();
+}
+
+function render() {
+  console.log('in render()');
+  $.ajax({
+    method: 'GET',
+    url: '/guesses'
+  }).then( (res) => {
+    console.log('we got it!', res);
+  }).catch( (err) => {
+    console.log('shit!');
+  })
+
 }
--- server/server.js
+++ server/server.js
@@ -8,9 +8,18 @@ app.use(bodyParser.urlencoded({extended:true}))
 
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
+const guessArray = ['some stuff'];
+
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (1 + max - min) + min);
+}
 
 // GET & POST Routes go here
-
+app.get('/guesses', (req, res) => {
+  console.log('retrieving guesses');
+  console.log(guessArray);
+  res.send(guessArray);
+})
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
